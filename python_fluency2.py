ccde1cf5dd1c0836779f49b8549a00a4527be7c1
Seal125 <stephaniesmith12514@gmail.com>
2020-01-22 15:06:41 +0000

    Complete, refactored

--- python_fluency2.py
+++ python_fluency2.py
@@ -1,42 +1,29 @@
 import re
 
 def halved(lst):
-    new_lst = []
-
-    for num in lst:
-        new_lst.append(num / 2)
-
-    return new_lst
+    return [num / 2 for num in lst]
 
 def only_positive(lst):
-    new_lst = []
-
-    for num in lst:
-        if num >= 0:
-            new_lst.append(num)
-
-    return new_lst
+    return [num for num in lst if num > 0]
 
 def total(lst):
     return sum(lst)
 
 def stripped_strings(lst):
-    new_lst = []
-
-    for word in lst:
-        new_lst.append(re.sub('[^A-Za-z]', '', word))
-
-    return new_lst
+    return [re.sub('\W', '', word) for word in lst]
 
 def find_special(lst):
     return [i for i in range(len(lst)) if lst[i] == 'special']
 
 def valid_contacts(lst):
-    return [lst[i] for i in range(len(lst)) if len(lst[i].phone_number) == 10]
+    return [contact for contact in lst if len(contact.phone_number) == 10]
 
-print(valid_contacts([
-        {"name": 'Reuben', "phone_number": '9196218777'},
-        {"name": 'Laisha', "phone_number": '0123334766'},
-        {"name": 'Cielo', "phone_number": '764'},
-        {"name": 'Maya', "phone_number": '7653324599'}
+def contact_names(lst):
+    return [contact['name'] for contact in lst]
+
+print(contact_names([
+        {name: 'Reuben', "phone_number": '9196218777'},
+        {name: 'Laisha', "phone_number": '0123334766'},
+        {name: 'Cielo', "phone_number": '764'},
+        {name: 'Maya', "phone_number": '7653324599'}
       ]))
